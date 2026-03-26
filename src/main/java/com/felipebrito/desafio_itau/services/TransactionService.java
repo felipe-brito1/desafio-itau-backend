394761292d106d28adfe123717c2ca487fb4d5a9
Felipe Brito <felipebrito0429@gmail.com>
2026-03-26 01:00:21 -0300

    Created TransactionService

--- src/main/java/com/felipebrito/desafio_itau/services/TransactionService.java
+++ src/main/java/com/felipebrito/desafio_itau/services/TransactionService.java
@@ -1,7 +1,30 @@
 package com.felipebrito.desafio_itau.services;
 
+import com.felipebrito.desafio_itau.model.Transaction;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Service
 public class TransactionService {
+    private final Queue<Transaction> transactionQueue = new ConcurrentLinkedQueue<>();
+
+    public void addTransactions(Transaction transaction){
+        transactionQueue.add(transaction);
+    }
+
+    public void clearTransactions(){
+        transactionQueue.clear();
+    }
+
+    public DoubleSummaryStatistics getStatistics(){
+        OffsetDateTime now = OffsetDateTime.now();
+        return transactionQueue.stream().filter(t -> t.getDataHora()
+                .isAfter(now.minusSeconds(60)))
+                .mapToDouble(Transaction::getValor)
+                .summaryStatistics();
+    }
 }
